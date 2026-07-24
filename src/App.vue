d136f4d3b3c145cc2f6c687bc05a5321b2609d72
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-07-24 08:58:10 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+<!-- ####################################### -->
+<!-- Exercise 1: Template Syntax -->
+<!-- ####################################### -->
 <!-- <script setup lang="ts">
 const msg = "Hello World";
 const num = 1;
@@ -13,6 +16,9 @@ const currentYear = new Date().getFullYear();
   <div>I am {{ currentYear - birthYear }} years old.</div>
 </template> -->
 
+<!-- ####################################### -->
+<!-- Exercise 2: Styles -->
+<!-- ####################################### -->
 <!-- <template>
   <div class="wrapper">
     <h2 class="header">Test CSS</h2>
@@ -21,15 +27,17 @@ const currentYear = new Date().getFullYear();
 
 <style scoped>
 .wrapper {
-  margin: 0.5em auto;
-  width: 95vh;
+  background-color: lightgray;
 }
 .header {
   color: teal;
 }
 </style> -->
 
-<!-- <script setup>
+<!-- ####################################### -->
+<!-- Exercise 3: Event Handling -->
+<!-- ####################################### -->
+<!-- <script setup lang="ts">
 function handleClick() {
   alert("Hello!");
 }
@@ -39,7 +47,22 @@ function handleClick() {
   <button @click="handleClick">Click</button>
 </template> -->
 
-<script setup>
+<!-- ####################################### -->
+<!-- Exercise 4: Component -->
+<!-- ####################################### -->
+<!-- <script setup lang="ts">
+import NavBar from "./Navbar.vue";
+</script>
+
+<template>
+  <NavBar />
+  <h1>Welcome to My App</h1>
+</template> -->
+
+<!-- ####################################### -->
+<!-- Exercise 5: State (not working)-->
+<!-- ####################################### -->
+<!-- <script setup>
 let counter = 0;
 function handleClick() {
   counter += 1;
@@ -50,4 +73,101 @@ function handleClick() {
 <template>
   <div>{{ counter }}</div>
   <button @click="handleClick">Add</button>
+</template> -->
+
+<!-- ####################################### -->
+<!-- Exercise 6: State (Working)-->
+<!-- ####################################### -->
+<!-- <script setup lang="ts">
+import { ref } from "vue";
+
+const counter = ref(0);
+
+function handleClick() {
+  counter.value += 1;
+}
+</script>
+
+<template>
+  <div>{{ counter }}</div>
+  <button @click="handleClick">Add</button>
+</template> -->
+
+<!-- ####################################### -->
+<!-- Exercise 7: Form 1 -->
+<!-- ####################################### -->
+<!-- <script setup lang="ts">
+import { ref } from "vue";
+
+const name = ref("");
+function handleInput(e) {
+  name.value = e.target.value;
+}
+
+function handleClick() {
+  name.value = "";
+}
+</script>
+
+<template>
+  <input @input="handleInput" :value="name" placeholder="Type your name" />
+  <p>Hello, {{ name }}</p>
+  <button @click="handleClick">Submit</button>
+</template> -->
+
+<!-- ####################################### -->
+<!-- Exercise 7: Form 2 -->
+<!-- ####################################### -->
+<!-- <script setup lang="ts">
+import { ref } from "vue";
+
+const name = ref("");
+function handleClick() {
+  name.value = "";
+}
+</script>
+
+<template>
+  <input v-model="name" placeholder="Type your name" />
+  <p>Hello, {{ name }}</p>
+  <button @click="handleClick">Submit</button>
+</template> -->
+
+<!-- ####################################### -->
+<!-- Exercise 8: List Rendering -->
+<!-- ####################################### -->
+
+<!-- <script setup lang="ts">
+import { ref } from "vue";
+const items = ref(["Item 1", "Item 2", "Item 3"]);
+</script>
+
+<template>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">
+      {{ index + 1 }} - {{ item }}
+    </li>
+  </ul>
+</template> -->
+
+<!-- ####################################### -->
+<!-- Exercise 9: Conditional Rendering -->
+<!-- ####################################### -->
+<script setup lang="ts">
+import { ref } from "vue";
+const toggle = ref(true);
+
+function handleClick() {
+  toggle.value = !toggle.value;
+}
+</script>
+
+<template>
+  <button @click="handleClick">Toggle</button>
+  <div v-if="toggle">
+    <p>Hello, World!</p>
+  </div>
+  <div v-else>
+    <p>Goodbye, World!</p>
+  </div>
 </template>
---
+++ src/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="nav">
+    <span>Home</span>
+    <span>About Me</span>
+  </div>
+</template>
+
+<style scoped>
+.nav {
+  display: flex;
+  gap: 0.5em;
+  background-color: lightgray;
+  padding: 0.2em;
+}
+</style>
